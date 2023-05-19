8daf8330c1b4ea16df14cc9178cbe15c90a84d4f
Marc Mata <marcmata26@gmail.com>
2023-05-19 09:50:50 -0500

    completed dao for users and usersAddr

---
+++ src/main/java/com/codeup/adlister/dao/UsersAddress.java
@@ -0,0 +1,9 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.UserAddress;
+
+public interface UsersAddress {
+    UserAddress findByCity(String city);
+
+    Long insert(UserAddress usersAddress);
+}
